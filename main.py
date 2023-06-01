ed3362485e63a093008f6339b86847f76c3f0523
CapMaverick <ytrapha@hotmail.com>
2023-06-01 18:03:28 -0300

    Listagem de cotações V1.0

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import json
 import locale
+import math
 
 def exibir_dados():
     with open('dados.json', 'r') as file:
@@ -12,10 +13,44 @@ def exibir_dados():
     # Configurar locale para o Brasil
     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
+    # Função para atualizar a tabela com os dados da página atual
+    def atualizar_tabela():
+        nonlocal lbl_pagina_atual  # Adicionado para tornar a variável acessível
+        table.delete(*table.get_children())
+        for stock in stocks[current_page * items_per_page: (current_page + 1) * items_per_page]:
+            close_formatted = locale.currency(stock['close'], grouping=True)
+            change_formatted = f"{stock['change']:.2f}%"
+            market_cap = stock['market_cap']
+            market_cap_formatted = locale.currency(market_cap, grouping=True) if market_cap is not None else ''
+
+            table.insert('', 'end', text='',
+                         values=(stock['stock'], stock['name'], close_formatted, change_formatted,
+                                 market_cap_formatted, stock['sector']))
+
+        lbl_pagina_atual.config(text=f"Página: {current_page + 1}/{total_pages}")
+
+    # Função para ir para a página anterior
+    def pagina_anterior():
+        nonlocal current_page
+        if current_page > 0:
+            current_page -= 1
+            atualizar_tabela()
+
+    # Função para ir para a próxima página
+    def proxima_pagina():
+        nonlocal current_page
+        if (current_page + 1) * items_per_page < len(stocks):
+            current_page += 1
+            atualizar_tabela()
+
     root = tk.Tk()
     root.title('Dados das Ações')
+    root.geometry('800x600')  # Definir tamanho fixo da janela
 
-    table = ttk.Treeview(root)
+    frame_tabela = tk.Frame(root)
+    frame_tabela.pack(fill=tk.BOTH, expand=True, padx=20, pady=(20, 0))  # Adicionado pady=(20, 0) para espaço apenas na parte superior
+
+    table = ttk.Treeview(frame_tabela)
     table['columns'] = ('Stock', 'Name', 'Close', 'Change', 'Market Cap', 'Sector')
 
     table.column('#0', width=0, stretch='no')
@@ -34,22 +69,26 @@ def exibir_dados():
     table.heading('Market Cap', text='Market Cap', anchor='center')
     table.heading('Sector', text='Sector', anchor='w')
 
-    for stock in stocks:
-        close_formatted = locale.currency(stock['close'], grouping=True)
-        change_formatted = f"{stock['change']:.2f}%"
-        market_cap = stock['market_cap']
-        market_cap_formatted = locale.currency(market_cap, grouping=True) if market_cap is not None else ''
+    table.pack(fill=tk.BOTH, expand=True)
 
-        table.insert('', 'end', text='', values=(
-            stock['stock'],
-            stock['name'],
-            close_formatted,
-            change_formatted,
-            market_cap_formatted,
-            stock['sector']
-        ))
+    # Paginação
+    items_per_page = 50
+    current_page = 0
+    total_pages = math.ceil(len(stocks) / items_per_page)
 
-    table.pack(expand=True, fill='both')
+    lbl_pagina_atual = tk.Label(root, text=f"Página: {current_page + 1}/{total_pages}")
+    lbl_pagina_atual.pack(pady=10)
+
+    frame_botoes = tk.Frame(root)
+    frame_botoes.pack()
+
+    btn_anterior = tk.Button(frame_botoes, text='Anterior', command=pagina_anterior)
+    btn_anterior.pack(side=tk.LEFT)
+
+    btn_proxima = tk.Button(frame_botoes, text='Próxima', command=proxima_pagina)
+    btn_proxima.pack(side=tk.LEFT)
+
+    atualizar_tabela()
 
     root.mainloop()
 
