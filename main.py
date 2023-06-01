77eb92b57bc8f23abf8f138df68c67ef309f298e
CapMaverick <ytrapha@hotmail.com>
2023-06-01 20:29:14 -0300

    Adiçao funcionalidade top 5

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import requests
 from tkinter import messagebox
 
+
 def obter_dados():
     response = requests.get('https://brapi.dev/api/quote/list')
     if response.status_code == 200:
@@ -17,26 +18,26 @@ def obter_dados():
         print('Falha ao obter dados da API')
         return None
 
+
 def salvar_historico():
-    # Verificar se a pasta "Histórico" existe, caso contrário, criar a pasta
     if not os.path.exists('Histórico'):
         os.makedirs('Histórico')
 
-    # Fazer uma cópia do arquivo de dados.json na pasta "Histórico" com um nome único
     shutil.copy('dados.json', os.path.join('Histórico', f'dados_{datetime.now().strftime("%Y%m%d%H%M%S")}.json'))
+
 
 def atualizar_dados():
     dados = obter_dados()
     if dados:
         with open('dados.json', 'w') as file:
             json.dump(dados, file, indent=4)
-        salvar_historico()  # Adicionado para salvar o histórico
+        salvar_historico()
         return True
     else:
         return False
 
+
 def exibir_dados():
-    # Verificar se o arquivo de dados.json existe, caso contrário, realizar a primeira atualização dos dados
     if not os.path.exists('dados.json'):
         if not atualizar_dados():
             return
@@ -46,12 +47,10 @@ def exibir_dados():
 
     stocks = dados['stocks']
 
-    # Configurar locale para o Brasil
     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-    # Função para atualizar a tabela com os dados da página atual
     def atualizar_tabela():
-        nonlocal lbl_pagina_atual  # Adicionado para tornar a variável acessível
+        nonlocal lbl_pagina_atual
         table.delete(*table.get_children())
         for stock in stocks[current_page * items_per_page: (current_page + 1) * items_per_page]:
             if stock['market_cap'] is not None and stock['sector'] is not None:
@@ -68,23 +67,65 @@ def exibir_dados():
 
         lbl_pagina_atual.config(text=f"Página: {current_page + 1}/{total_pages}")
 
-    # Função para ir para a página anterior
     def pagina_anterior():
         nonlocal current_page
         if current_page > 0:
             current_page -= 1
             atualizar_tabela()
 
-    # Função para ir para a próxima página
     def proxima_pagina():
         nonlocal current_page
         if (current_page + 1) * items_per_page < len(stocks):
             current_page += 1
             atualizar_tabela()
 
+    def analisar_variacao():
+        top_5_variacao = sorted(stocks, key=lambda x: x['change'], reverse=True)[:5]
+
+        top5_window = tk.Toplevel(root)
+        top5_window.title('Top 5 Variação')
+        top5_window.geometry('800x600')
+
+        frame_tabela_top5 = tk.Frame(top5_window)
+        frame_tabela_top5.pack(fill=tk.BOTH, expand=True, padx=20, pady=(20, 0))
+
+        table_top5 = ttk.Treeview(frame_tabela_top5)
+        table_top5['columns'] = ('Stock', 'Name', 'Close', 'Change', 'Market Cap', 'Sector')
+
+        table_top5.column('#0', width=0, stretch='no')
+        table_top5.column('Stock', anchor='center', width=80)
+        table_top5.column('Name', anchor='w', width=200)
+        table_top5.column('Close', anchor='center', width=100)
+        table_top5.column('Change', anchor='center', width=100)
+        table_top5.column('Market Cap', anchor='center', width=120)
+        table_top5.column('Sector', anchor='w', width=200)
+
+        table_top5.heading('#0', text='', anchor='w')
+        table_top5.heading('Stock', text='Stock', anchor='center')
+        table_top5.heading('Name', text='Name', anchor='w')
+        table_top5.heading('Close', text='Close', anchor='center')
+        table_top5.heading('Change', text='Change', anchor='center')
+        table_top5.heading('Market Cap', text='Market Cap', anchor='center')
+        table_top5.heading('Sector', text='Sector', anchor='w')
+
+        table_top5.pack(fill=tk.BOTH, expand=True)
+
+        for stock in top_5_variacao:
+            if stock['market_cap'] is not None and stock['sector'] is not None:
+                close_formatted = locale.currency(stock['close'], grouping=True)
+                change = stock['change']
+                change_formatted = f"{change:.2f}%"
+                market_cap = stock['market_cap']
+                market_cap_formatted = locale.currency(market_cap, grouping=True)
+                sector = stock['sector']
+
+                table_top5.insert('', 'end', text='',
+                                  values=(stock['stock'], stock['name'], close_formatted, change_formatted,
+                                          market_cap_formatted, sector))
+
     root = tk.Tk()
     root.title('Dados das Ações')
-    root.geometry('800x600')  # Definir tamanho fixo da janela
+    root.geometry('800x600')
 
     frame_tabela = tk.Frame(root)
     frame_tabela.pack(fill=tk.BOTH, expand=True, padx=20, pady=(20, 0))
@@ -110,7 +151,6 @@ def exibir_dados():
 
     table.pack(fill=tk.BOTH, expand=True)
 
-    # Paginação
     items_per_page = 50
     current_page = 0
     total_pages = math.ceil(len(stocks) / items_per_page)
@@ -127,9 +167,11 @@ def exibir_dados():
     btn_proxima = tk.Button(frame_botoes, text='Próxima', command=proxima_pagina)
     btn_proxima.pack(side=tk.LEFT)
 
+    btn_variacao = tk.Button(frame_botoes, text='Top 5 Variação', command=analisar_variacao)
+    btn_variacao.pack(side=tk.LEFT)
+
     atualizar_tabela()
 
-    # Verificar se houve atualização dos dados e mostrar mensagem de confirmação
     if atualizar_dados():
         messagebox.showinfo('Atualização', 'Os dados foram atualizados com sucesso e o arquivo antigo foi salvo no histórico.')
     else:
